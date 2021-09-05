4a1f33b4112c27ed2ebf5f8f9093eee52f8d99f3
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-05 19:43:12 -0300

    Adicionando color-picker nas tiras

---
+++ script.js
@@ -0,0 +1,5 @@
+$(".strip").colorPick({
+  'allowRecent': false,
+  'allowCustomColor': false,
+  'palette': ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'gray', 'white', 'goldenrod', 'silver']
+});
